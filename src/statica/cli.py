87c01b93a6e00a489dfb37f65d5d63d46f810d86
ryoshu404 <192475624+ryoshu404@users.noreply.github.com>
2026-03-03 12:15:24 -0600

    feat: validate file input

--- src/statica/cli.py
+++ src/statica/cli.py
@@ -1,10 +1,26 @@
 import argparse
+import sys
+from pathlib import Path
 
 def parse_args():
-    pass
+    parser = argparse.ArgumentParser(
+        prog='Statica',
+        description='Statica is a format-agnostic analysis pipeline written in Python',
+        epilog='github.com/ryoshu404/statica'
+    )
+    parser.add_argument('path')
+    parser.add_argument('-f', '--format', choices=['json'], default='json', help="Format for output, defaults to json.")
+    return parser.parse_args()
 
 def main():
-    pass
+    args = parse_args()
+    filepath = Path(args.path)
+    if filepath.exists() and filepath.is_file():
+        print("valid file")
+    else:
+        print(f"Error: {filepath} not found", file=sys.stderr)
+        sys.exit(1)
+    
 
 if __name__ == "__main__":
     main()
